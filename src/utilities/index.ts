c08854b5afa439473ae9ebad8bf02cfbb98990fd
David Kushner <david.kushner@byton.com>
2018-09-15 14:40:39 -0700

    Prepare for initial publish.

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,95 +1,95 @@
-import * as _ from 'lodash'
+import * as _ from 'lodash';
 
 const recursiveSetIn = (state: any | undefined, index: number, path: string[], value: any): any => {
   // Base case where recursion has reached limit.
   if (index >= path.length) {
-    return value
+    return value;
   }
 
-  const key = path[index]
+  const key = path[index];
 
   if (!_.isNumber(key)) {
     if (state === undefined || state === null) {
-      const result = recursiveSetIn(undefined, index + 1, path, value)
-      return result === undefined ? undefined : { [key]: result }
+      const result = recursiveSetIn(undefined, index + 1, path, value);
+      return result === undefined ? undefined : { [key]: result };
     }
 
     if (_.isArray(state)) {
-      throw new Error('Cannot set non-numeric property on array.')
+      throw new Error('Cannot set non-numeric property on array.');
     }
 
-    const result = recursiveSetIn(state[key], index + 1, path, value)
-    const keyCount = Object.keys(state).length
+    const result = recursiveSetIn(state[key], index + 1, path, value);
+    const keyCount = Object.keys(state).length;
 
     if (result === undefined) {
       if (state[key] === undefined && keyCount === 0) {
-        return undefined
+        return undefined;
       }
 
       if (state[key] !== undefined && keyCount <= 1) {
         if (!_.isNaN(path[index - 1])) {
-          return { }
+          return {};
         } else {
-          return undefined
+          return undefined;
         }
       }
     }
 
     return {
       ...state,
-      [key]: result
-    }
+      [key]: result,
+    };
   }
 
-  const numericKey = Number(key)
+  const numericKey = Number(key);
   if (state === undefined || state === null) {
-    const result = recursiveSetIn(undefined, index + 1, path, value)
+    const result = recursiveSetIn(undefined, index + 1, path, value);
 
     if (result === undefined) {
-      return undefined
+      return undefined;
     }
 
-    const array = []
-    array[numericKey] = result
-    return array
+    const array = [];
+    array[numericKey] = result;
+    return array;
   }
 
   if (!_.isArray(state)) {
-    throw new Error('Cannot set a numeric property on an object.')
+    throw new Error('Cannot set a numeric property on an object.');
   }
 
-  const existingValue = state[numericKey]
-  const result = recursiveSetIn(existingValue, index + 1, path, value)
+  const existingValue = state[numericKey];
+  const result = recursiveSetIn(existingValue, index + 1, path, value);
 
-  const array = [...state]
-  array[numericKey] = result
-  return array
-}
+  const array = [...state];
+  array[numericKey] = result;
+  return array;
+};
 
 export const setIn = (state: any, key: string, value: any) => {
   if (state === undefined || state === null) {
-    throw new Error(`Cannot set property in ${String(state)} state.`)
+    throw new Error(`Cannot set property in ${String(state)} state.`);
   }
 
   if (key === undefined || key === null) {
-    throw new Error(`Cannot set property with ${String(key)} key.`)
+    throw new Error(`Cannot set property with ${String(key)} key.`);
   }
 
-  return recursiveSetIn(state, 0, _.toPath(key), value)
-}
+  return recursiveSetIn(state, 0, _.toPath(key), value);
+};
 
 export const getIn = (state: any, compound: string) => {
-  const path = _.toPath(compound)
+  const path = _.toPath(compound);
 
-  let current = state
+  let current = state;
   for (const key of path) {
-    const arrayInvalid = (_.isArray(current) && !_.isNumber(key))
+    const arrayInvalid = _.isArray(current) && !_.isNumber(key);
     if (current === undefined || current === null || !_.isObject(current) || arrayInvalid) {
-      return undefined
+      return undefined;
     }
 
-    current = current[key]
+    current = current[key];
   }
 
-  return current
-}
+  return current;
+};
--- src/utilities/logging.ts
+++ src/utilities/logging.ts
@@ -4,78 +4,74 @@
  * @param vm Component instance that encountered the error
  * @param parent Parent to the faulting component
  */
-function createMessage (message: string, vm?: any, parent?: any): string | void {
+function createMessage(message: string, vm?: any, parent?: any): string | void {
   if (parent) {
     vm = {
       $options: vm,
       $parent: parent,
-      _isVue: true
-    }
+      _isVue: true,
+    };
   }
 
   if (vm) {
-    vm.$_logged = vm.$_logged || []
+    vm.$_logged = vm.$_logged || [];
 
     if (vm.$_logged.includes(message)) {
-      return
+      return;
     }
 
-    vm.$_logged.push(message)
+    vm.$_logged.push(message);
   }
 
-  return `[Bureau] ${message}` + (vm ? componentTrace(vm) : '')
+  return `[Bureau] ${message}` + (vm ? componentTrace(vm) : '');
 }
 
-export function info (message: string, vm?: any, parent?: any): void {
-  const wrapped = createMessage(message, vm, parent)
-  wrapped != null && console.info(wrapped)
+export function info(message: string, vm?: any, parent?: any): void {
+  const wrapped = createMessage(message, vm, parent);
+  wrapped != null && console.info(wrapped);
 }
 
-export function warn (message: string, vm?: any, parent?: any): void {
-  const wrapped = createMessage(message, vm, parent)
-  wrapped != null && console.warn(wrapped)
+export function warn(message: string, vm?: any, parent?: any): void {
+  const wrapped = createMessage(message, vm, parent);
+  wrapped != null && console.warn(wrapped);
 }
 
-export function error (message: string, vm?: any, parent?: any): void {
-  const wrapped = createMessage(message, vm, parent)
-  wrapped != null && console.error(wrapped)
+export function error(message: string, vm?: any, parent?: any): void {
+  const wrapped = createMessage(message, vm, parent);
+  wrapped != null && console.error(wrapped);
 }
 
-export function deprecate (original: string, replacement: string, vm?: any, parent?: any) {
-  warn(`'${original}' is deprecated. Use '${replacement}' instead.`, vm, parent)
+export function deprecate(original: string, replacement: string, vm?: any, parent?: any) {
+  warn(`'${original}' is deprecated. Use '${replacement}' instead.`, vm, parent);
 }
 
 /**
  * The following is stolen from Vue's own internal debugging code.
  */
-const classifyRE = /(?:^|[-_])(\w)/g
-const classify = (str: string) => str
-  .replace(classifyRE, c => c.toUpperCase())
-  .replace(/[-_]/g, '')
+const classifyRE = /(?:^|[-_])(\w)/g;
+const classify = (str: string) => str.replace(classifyRE, c => c.toUpperCase()).replace(/[-_]/g, '');
 
-function formatComponentName (vm: any, includeFile?: boolean): string {
+function formatComponentName(vm: any, includeFile?: boolean): string {
   if (vm.$root === vm) {
-    return '<Root>'
+    return '<Root>';
   }
 
-  const options = typeof vm === 'function' && vm.cid != null
-    ? vm.options
-    : vm._isVue
-      ? vm.$options || vm.constructor.options
-      : vm || {}
+  const options =
+    typeof vm === 'function' && vm.cid != null
+      ? vm.options
+      : vm._isVue
+        ? vm.$options || vm.constructor.options
+        : vm || {};
 
-  let name = options.name || options._componentTag
-  const file = options.__file
+  let name = options.name || options._componentTag;
+  const file = options.__file;
 
   if (!name && file) {
-    const match = file.match(/([^/\\]+)\.vue$/)
-    name = match && match[1]
+    const match = file.match(/([^/\\]+)\.vue$/);
+    name = match && match[1];
   }
 
-  return (
-    (name ? `<${classify(name)}>` : `<Anonymous>`) +
-    (file && includeFile !== false ? ` at ${file}` : '')
-  )
+  return (name ? `<${classify(name)}>` : `<Anonymous>`) + (file && includeFile !== false ? ` at ${file}` : '');
 }
 
 /**
@@ -83,39 +79,44 @@ function formatComponentName (vm: any, includeFile?: boolean): string {
  * @param vm Component instance to trace
  */
 /* tslint:disable */
-function componentTrace (vm: any): string {
+function componentTrace(vm: any): string {
   if (vm._isVue && vm.$parent) {
-    const tree = []
-    let currentRecursiveSequence = 0
+    const tree = [];
+    let currentRecursiveSequence = 0;
 
     while (vm) {
       if (tree.length > 0) {
-        const last: any = tree[tree.length - 1]
+        const last: any = tree[tree.length - 1];
 
         if (last.constructor === vm.constructor) {
-          currentRecursiveSequence++
-          vm = vm.$parent
-          continue
+          currentRecursiveSequence++;
+          vm = vm.$parent;
+          continue;
         } else if (currentRecursiveSequence > 0) {
-          tree[tree.length - 1] = [last, currentRecursiveSequence]
-          currentRecursiveSequence = 0
+          tree[tree.length - 1] = [last, currentRecursiveSequence];
+          currentRecursiveSequence = 0;
         }
       }
 
-      tree.push(vm)
-      vm = vm.$parent
+      tree.push(vm);
+      vm = vm.$parent;
     }
 
-    return '\n\nfound in\n\n' + tree.map((vm, i) => `${
-        i === 0 ? '---> ' : ' '.repeat(5 + i * 2)
-      }${
-        Array.isArray(vm)
-          ? `${formatComponentName(vm[0])}... (${vm[1]} recursive calls)`
-          : formatComponentName(vm)
-      }`)
-      .join('\n')
+    return (
+      '\n\nfound in\n\n' +
+      tree
+        .map(
+          (vm, i) =>
+            `${i === 0 ? '---> ' : ' '.repeat(5 + i * 2)}${
+              Array.isArray(vm)
+                ? `${formatComponentName(vm[0])}... (${vm[1]} recursive calls)`
+                : formatComponentName(vm)
+            }`,
+        )
+        .join('\n')
+    );
   } else {
-    return `\n\n(found in ${formatComponentName(vm)})`
+    return `\n\n(found in ${formatComponentName(vm)})`;
   }
   /* tslint:enable */
 }
